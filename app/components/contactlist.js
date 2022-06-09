be7baa009ad4b13e728aa52a6fb2751c61d86d66
jsz4n <jsz4n@laposte.net>
2022-06-09 15:47:12 +0200

    begin refactoring

--- app/components/contactlist.js
+++
@@ -1,8 +0,0 @@
-import Component from '@glimmer/component';
-import { action } from '@ember/object';
-import { service } from '@ember/service';
-
-export default class Mapr1Component extends Component {
-  @service store;
-  contactpoints = this.store.findAll('contactpoint');
-}
