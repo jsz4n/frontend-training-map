b4f16bd60c1830d9ba2be7ad9c84c1d5574be11e
jsz4n <jsz4n@laposte.net>
2022-06-09 17:04:17 +0200

    add contactpoints model

--- app/models/contactpoint.js
+++ app/models/contactpoint.js
@@ -1,8 +1,42 @@
-import Model, { attr,  belongsTo } from '@ember-data/model';
+import Model, { attr, belongsTo } from '@ember-data/model';
 
 export default class contactpointModel extends Model {
   @attr mail;
   @attr phone;
   //@attr address;
   @belongsTo('address') address;
+
+  get email(){
+      return this.mail.toLowerCase();
+  }
+
+  get hasMail(){
+      return (this.mail) && !(this.mail === "");
+  }
+  get hasPhone(){
+      return (this.phone) && !(this.phone === "");
+  }
+
+  get isPerson(){
+      let [names,domain] = this.mail.split("@");
+      return names.indexOf(".")!=-1;
+  }
+  get firstname(){
+      let fname = this.mail.split(".")[0].toLowerCase();
+      let firstC=fname.charAt(0).toUpperCase()
+      return firstC+fname.substring(1);
+  }
+
+  get lastname(){
+      return this.mail.split(".")[1].split("@")[0].toUpperCase();
+  }
+
+  get telephone(){
+      if (this.phone){
+          let p= this.phone.replace(/\s+|\/+|[a-z]+|[A-Z]+|\./g,"");
+          p = (p.charAt(0)=="+")?p:(p.charAt(0)=="0"?"+32"+p.slice(1):"+32"+p);
+          return p;
+      }
+      return "";
+  }
 }
