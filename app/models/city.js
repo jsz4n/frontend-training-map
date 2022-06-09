64f5f7b5eacd60786a15a3dff645b78f1df458a2
jsz4n <jsz4n@laposte.net>
2022-06-09 15:49:39 +0200

    change model and simplify application template

--- app/models/city.js
+++ app/models/city.js
@@ -1,9 +1,9 @@
-import Model, { attr, belongsTo } from '@ember-data/model';
+import Model, { attr, belongsTo, hasMany } from '@ember-data/model';
 
 export default class CityModel extends Model {
   @attr name;
   @attr lat;
   @attr long;
   @attr postcode;
-  @belongsTo('address') address;
+  @hasMany('address') addresses;
 }
